75456a2d69ba2312a2be88b87fb1348581e1a77f
Fernando Celmer <fernandocelmer@gmail.com>
2019-04-05 02:44:50 -0300

    Create wpfc-page.php

---
+++ plugin-teste-wpfc/includes/wpfc-page.php
@@ -0,0 +1,6 @@
+<div class="wrap">
+  <h1>Plugin Teste WPFC</h1>
+  <p>Plugin básico Wordpress com uma página de administração para o desenvolvimento de projetos e novas funcionalidades...</p>
+  <p>Site: <a href="www.fernandocelmer.com">www.fernandocelmer.com</a></p>
+  <p>E-mail: <a href="www.fernandocelmer.com">email@fernandocelmer.com</p>
+</div>
