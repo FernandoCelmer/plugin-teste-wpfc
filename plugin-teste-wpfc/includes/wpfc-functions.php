47edcca12152932b4c9fdf9efa38c787972c150c
Fernando Celmer <fernandocelmer@gmail.com>
2019-04-05 02:44:48 -0300

    Create wpfc-functions.php

---
+++ plugin-teste-wpfc/includes/wpfc-functions.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Add my new menu to the Admin Control Panel
+ */
+ 
+// Hook the 'admin_menu' action hook, run the function named 'wpfc_Add_My_Admin_Link()'
+add_action( 'admin_menu', 'wpfc_Add_My_Admin_Link' );
+ 
+// Add a new top level menu link to the ACP
+function wpfc_Add_My_Admin_Link()
+{
+      add_menu_page(
+        'Plugin Teste WPFC', // Title of the page
+        'WPFC', // Text to show on the menu link
+        'manage_options', // Capability requirement to see the link
+        'plugin-teste-wpfc/includes/wpfc-page.php'
+    );
+  }
+
+?>
