4d4d3170042a1a5274439a076c76d01a875b19e1
Fernando Celmer <fernandocelmer@gmail.com>
2019-04-05 02:44:52 -0300

    Create plugin-teste-wpfc.php

---
+++ plugin-teste-wpfc/plugin-teste-wpfc.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Plugin Teste WPFC
+Description: Plugin básico Wordpress com uma página de administração para o desenvolvimento de projetos e novas funcionalidades...
+Author: Fernando Celmer
+Version: 0.1
+*/
+// Include wpfc-functions.php, use require_once to stop the script if wpfc-functions.php is not found
+require_once plugin_dir_path(__FILE__) . 'includes/wpfc-functions.php';
+?>
